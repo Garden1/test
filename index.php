6190d7c9308375c2df341ce42d4e4c62746717ea
Your Name <ventura1234@mail.ru>
2017-11-28 10:11:17 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+   <form action="action.php" method="post">
+ <p>Ваше имя: <input type="text" name="name" /></p>
+ <p>Ваш возраст: <input type="text" name="age" /></p>
+ <p><input type="submit" /></p>
+</form>
+   
+    <?php echo & message;?><br />
+</body>
+</html>
